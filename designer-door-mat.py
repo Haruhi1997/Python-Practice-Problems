d88d130a89c07c2a5b98271ad3a5e61f2d116341
Nancy Scarlett <nancyscarlett1997@gmail.com>
2018-07-08 11:30:56 +0530

    deleted unnecessary files

--- designer-door-mat.py
+++
@@ -1,7 +0,0 @@
-N,M = map(int,input().split())
-
-pattern = [('.|.'*(2*i+1)).center(M,'-') for i in range(N//2)]
-
-print('\n'.join(pattern+['WELCOME'.center(M,'-')]+pattern[::-1]))
-
-#Here str.center(width,fillcharacter) makes the str string in the center of a st#ring of width = 'width' and fills the remaning length of the string with the fil#l character
